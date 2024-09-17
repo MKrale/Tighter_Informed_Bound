8d4bcefc8bdae0be6f02cb3c1ebd1178c4a356bb
MKrale <merlijnkrale@gmail.com>
2024-09-17 11:21:26 +0200

    benchmark resuls 3.1

--- RunAll.sh
+++ RunAll.sh
@@ -8,29 +8,29 @@ nmbr_cores=100
 #        Snake Maze Environment:
 ##########################################################
 
-echo -e "\n\n============= UPPER BOUND TESTS  =============\n\n"
+#echo -e "\n\n============= UPPER BOUND TESTS  =============\n\n"
 
 folder_path="Data/UpperBounds/"
 
 # for env in "ABC" "Tiger" "RockSample5" "RockSample10" "K-out-of-N2" "K-out-of-N3" "FrozenLake4" "FrozenLake10" "Hallway1" "Hallway2" "MiniHallway" "TigerGrid" "Tag" # ALL
-for env in "ABC" "RockSample5" "FrozenLake4" "Tiger" # QUICK
-# for env in "RockSample10" "K-out-of-N2" "K-out-of-N3" "FrozenLake10" "Tag" "SparseHallway1" "SparseHallway2" "SparseTigerGrid"  # LONG
-do
-  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.95 &
-  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.95 --solver WBIB &
-  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.99 &
-  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.99 --solver WBIB &
-done
-wait
+#for env in "ABC" "RockSample5" "FrozenLake4" "Tiger" # QUICK
+#for env in "RockSample10" "K-out-of-N2" "K-out-of-N3" "FrozenLake10" "Tag" "SparseHallway1" "SparseHallway2" "SparseTigerGrid"  # LONG
+#do
+#  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.95 &
+#  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.95 --solver WBIB &
+#  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.99 &
+#  julia --project=. run_upperbound.jl --env $env --path $folder_path --discount 0.99 --solver WBIB &
+#done
+# wait
 
-echo -e "\n\n============= SARSOP TESTS  =============\n\n"
+ echo -e "\n\n============= SARSOP TESTS  =============\n\n"
 
 folder_path="Data/SarsopTest/"
 i=0
 
 # for env in "ABC" "Tiger" "RockSample5" "RockSample10" "K-out-of-N2" "K-out-of-N3" "FrozenLake4" "FrozenLake10" "SparseHallway1" "SparseHallway2" "SparseTigerGrid" "Tag" # ALL
-for env in "ABC" "RockSample5" "FrozenLake4" "Tiger" # QUICK
-# for env in "RockSample10" "K-out-of-N2" "K-out-of-N3" "FrozenLake10" "Tag" "SparseHallway1" "SparseHallway2" "SparseTigerGrid" # LONG
+#for env in "ABC" "RockSample5" "FrozenLake4" "Tiger" # QUICK
+ for env in "RockSample10" "K-out-of-N2" "K-out-of-N3" "FrozenLake10" "Tag" "SparseHallway1" "SparseHallway2" "SparseTigerGrid" # LONG
 # for env in "Hallway1" "Hallway2" "MiniHallway" "TigerGrid"
 # for env in "K-out-of-N3" "FrozenLake10"
 do
